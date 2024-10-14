482d96e502fc05a9bc3fc9ef76ee634f0d83850d
prabhsuratsingh <prabhsurat17@gmail.com>
2024-10-14 18:31:18 +0530

    setup basic configs for app

---
+++ app/src/main/java/com/example/graphsudoku/domain/Difficulty.kt
@@ -0,0 +1,7 @@
+package com.example.graphsudoku.domain
+
+enum class Difficulty(val modifier: Double) {
+    EASY(0.5),
+    MEDIUM(0.44),
+    HARD(0.38)
+}
---
+++ app/src/main/java/com/example/graphsudoku/domain/Settings.kt
@@ -0,0 +1,6 @@
+package com.example.graphsudoku.domain
+
+data class Settings(
+    val difficulty: Difficulty,
+    val boundary: Int
+)

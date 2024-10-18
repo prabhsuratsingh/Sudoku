1a2cd955e7dd54a170e5916bc47962f97e7f867c
prabhsuratsingh <prabhsurat17@gmail.com>
2024-10-18 22:00:39 +0530

    added dispatcher providers

---
+++ app/src/main/java/com/example/graphsudoku/common/DispatcherProvider.kt
@@ -0,0 +1,8 @@
+package com.example.graphsudoku.common
+
+import kotlin.coroutines.CoroutineContext
+
+interface DispatcherProvider {
+    fun provideUIContext() : CoroutineContext
+    fun provideIOContext() : CoroutineContext
+}

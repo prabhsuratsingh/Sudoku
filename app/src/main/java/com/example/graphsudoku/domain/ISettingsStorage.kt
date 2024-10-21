fec957fe63d1af40a5938640a8cfd8deb54d7ab5
prabhsuratsingh <prabhsurat17@gmail.com>
2024-10-21 23:01:38 +0530

    modified settings storage and game repository

--- app/src/main/java/com/example/graphsudoku/domain/ISettingsStorage.kt
+++ app/src/main/java/com/example/graphsudoku/domain/ISettingsStorage.kt
@@ -9,5 +9,6 @@ interface ISettingsStorage {
 
 sealed class SettingsStorageResult {
     data class OnSuccess(val settings: Settings) : SettingsStorageResult()
+    data object OnComplete : SettingsStorageResult()
     data class OnError(val exception: Exception) : SettingsStorageResult()
 }
